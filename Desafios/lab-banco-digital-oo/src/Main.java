6a35d4213ecddf2e820f688a94e8a27126e85d4c
jpgfigueired0 <contasfigueiredo77@gmail.com>
2024-07-08 15:41:23 -0300

    feat: (DESAFIO) Tests pela classe Main

--- Desafios/lab-banco-digital-oo/src/Main.java
+++ Desafios/lab-banco-digital-oo/src/Main.java
@@ -1,18 +1,33 @@
-
+/**
+ * Classe principal para testar as operações bancárias.
+ */
 public class Main {
 
     public static void main(String[] args) {
-        Cliente venilton = new Cliente();
-        venilton.setNome("Cliente Teste");
+        Banco banco = new Banco();
+        banco.setNome("Banco Teste");
 
-        Conta cc = new ContaCorrente(venilton);
-        Conta poupanca = new ContaPoupanca(venilton);
+        Cliente cliente1 = new Cliente();
+        cliente1.setNome("Cliente Teste 1");
+
+        Conta cc = new ContaCorrente(cliente1);
+        Conta poupanca = new ContaPoupanca(cliente1);
+
+        banco.adicionarConta(cc);
+        banco.adicionarConta(poupanca);
 
         cc.depositar(100);
-        cc.transferir(100, poupanca);
+        cc.transferir(50, poupanca);
+
+//        Teste de saque valor excedente
+//        cc.sacar(500);
 
         cc.imprimirExtrato();
         poupanca.imprimirExtrato();
-    }
 
+        ((ContaPoupanca) poupanca).aplicarRendimento(0.01);
+        poupanca.imprimirExtrato();
+
+        banco.listarContas();
+    }
 }
