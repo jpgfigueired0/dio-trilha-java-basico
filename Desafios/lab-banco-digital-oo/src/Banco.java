d9e88d7a988d9d36d768971d16a1c14d08b9f098
jpgfigueired0 <contasfigueiredo77@gmail.com>
2024-07-08 15:15:18 -0300

    feat: (DESAFIO) Criando um banco digital com java e oriantação a objetos

---
+++ Desafios/lab-banco-digital-oo/src/Banco.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Banco {
+
+    private String nome;
+    private List<Conta> contas;
+
+    public Banco() {
+        this.contas = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+
+    //métodos para gerenciar contas, como adicionar, remover, buscar contas, buscar por cliente e listar todas contas
+    public void adicionarConta(Conta conta) {
+        this.contas.add(conta);
+    }
+
+    public void removerConta(Conta conta) {
+        this.contas.remove(conta);
+    }
+
+    public Conta buscarConta(int numero) {
+        return this.contas.stream()
+                .filter(conta -> conta.getNumero() == numero)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Optional<Conta> buscarPorCliente(String nomeCliente) {
+        return this.contas.stream()
+                .filter(conta -> conta.getCliente().getNome().equals(nomeCliente))
+                .findFirst();
+    }
+
+    public void listarContas() {
+        this.contas.forEach(conta -> {
+            System.out.println("Cliente" + conta.getCliente().getNome() + " | Conta: " + conta.getNumero());
+        });
+    }
+}
